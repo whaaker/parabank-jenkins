c0aaf5ed9298c53cb04549fb7cfc4f43064492d1
pteodor <pteodor@parasoft.com>
2024-10-15 13:30:23 +0200

    removed Parabank, leaving Selenium tests only

--- selenic/src/main/java/com/parasoft/bookstore/CartTimer.java
+++
@@ -1,9 +0,0 @@
-package com.parasoft.bookstore;
-
-// public class CartTimer extends TimerTask {
-public class CartTimer implements Runnable {
-    @Override
-    public void run() {
-        new CartService().removeExpiredOrdersAndBooks();
-    }
-}
--- selenic/src/main/java/com/parasoft/parabank/service/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(
-        elementFormDefault=javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.parasoft.parabank.service;
